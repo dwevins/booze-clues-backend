971bd431048feecc4b62beea798dd77fff0059e6
James Ridley <jamesridley3@gmail.com>
2016-08-26 15:29:58 -0500

    ryan corrected some things

--- app/Http/JsonApiViews/RecipeIngredient.js
+++ app/Http/JsonApiViews/RecipeIngredient.js
@@ -1,16 +1,26 @@
 const JsonApiView = require('adonis-jsonapi/src/JsonApiView');
 
 class RecipeIngredient extends JsonApiView {
+  get type() {
+    return 'recipe-ingredients';
+  }
+
   get attributes() {
     return ['quantity', 'measure', 'addb_measure'];
   }
 
   drink() {
-    return this.belongsTo('App/Http/JsonApiViews/Drink', true);
+    return this.belongsTo('App/Http/JsonApiViews/Drink', {
+      included: true,
+      excludeRelation: 'recipeIngredients',
+    });
   }
 
   ingredient() {
-    return this.belongsTo('App/Http/JsonApiViews/Ingredient', true);
+    return this.belongsTo('App/Http/JsonApiViews/Ingredient', {
+      included: true,
+      excludeRelation: 'recipeIngredients',
+    });
   }
 }
 
