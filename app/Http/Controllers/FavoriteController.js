158dfef2996141873c936f025088a65719097763
Danny Evins <dcastkid@gmail.com>
2016-08-30 11:18:57 -0500

    had to un-comment

--- app/Http/Controllers/FavoriteController.js
+++ app/Http/Controllers/FavoriteController.js
@@ -5,45 +5,49 @@ const Drink = use('App/Model/Drink');
 
 class FavoriteController {
 
-  // * index(request, response) {
-  //   const favorites = yield Favorite.with('user', 'drink').fetch();
-  //
-  //   response.jsonApi('Favorite', favorites);
-  // }
-  //
-  // * store(request, response) {
-  //   const input = request.jsonApi.getAttributesSnakeCase(attributes);
-  //   const foreignKeys = {
-  //     user_id: user,
-  //     drink_id: drink,
-  //   };
-  //   const favorite = yield Favorite.create(Object.assign({}, input, foreignKeys));
-  //
-  //   response.jsonApi('Favorite', favorite);
-  // }
+  * index(request, response) {
+    const favorites = yield Favorite.with('user', 'drink').fetch();
 
-  * show(request, response) {
-    const userID = request.param('id');
-    // const favorite = yield Favorite.with('user', 'drink').where({ userID }).firstOrFail();
-    const favorite
+    response.jsonApi('Favorite', favorites);
+  }
+
+  * store(request, response) {
+    const input = request.jsonApi.getAttributesSnakeCase(attributes);
+    const foreignKeys = {
+      user_id: user,
+      drink_id: drink,
+    };
+    const favorite = yield Favorite.create(Object.assign({}, input, foreignKeys));
 
     response.jsonApi('Favorite', favorite);
   }
 
-  // * update(request, response) {
-  //   const id = request.param('id');
-  //   request.jsonApi.assertId(id);
-  //
-  //   const input = request.jsonApi.getAttributesSnakeCase(attributes);
-  //   const foreignKeys = {
-  //     user_id: user,
-  //     drink_id: drink,
-  //   };
-    //
-    // const favorite = yield Favorite.with('user', 'drink').where({ id }).firstOrFail();
-    // yield favorite.update(Object.assign({}, input, foreignKeys));
-    //
-    // response.send(favorite);
+  * show(request, response) {
+    const userID = request.param('id');
+    // const favorite = yield Favorite.with('user', 'drink').where({ userID }).firstOrFail();
+    const favorite = yield Drink.with('creator', 'recipe_ingredients.ingredient')
+      .select('drinks.*')
+      .join('favorites', 'drinks.id', 'favorites.drink_id')
+      .where('favorites.user_id', `%${userID}%`)
+      .orderBy('favorites.created_at', 'desc');
+
+    response.jsonApi('Favorite', favorite);
+  }
+
+  * update(request, response) {
+    const id = request.param('id');
+    request.jsonApi.assertId(id);
+
+    const input = request.jsonApi.getAttributesSnakeCase(attributes);
+    const foreignKeys = {
+      user_id: user,
+      drink_id: drink,
+    };
+
+    const favorite = yield Favorite.with('user', 'drink').where({ id }).firstOrFail();
+    yield favorite.update(Object.assign({}, input, foreignKeys));
+
+    response.send(favorite);
   }
 
   * destroy(request, response) {
