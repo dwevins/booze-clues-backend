a8d32a8650e40a8eeb2781d6f8708556c04655a5
Danny Evins <dcastkid@gmail.com>
2016-08-31 10:24:35 -0500

    duplicate entries are prevented

--- app/Http/Controllers/FavoriteController.js
+++ app/Http/Controllers/FavoriteController.js
@@ -14,15 +14,25 @@ class FavoriteController {
 
   * store(request, response) {
     const attrs = yield request.jsonApi.getAttributesSnakeCase(attributes);
+
     const foreignKeys = {
       user_id: request.authUser.id,
       drink_id: request.input('data.relationships.drink.data.id'),
     };
 
-    const favorite = yield Favorite.create(Object.assign({}, attrs, foreignKeys));
-    yield favorite.related('user').load();
 
-    response.jsonApi('Favorite', favorite);
+    const duplicateCheckTable = yield Favorite.with()
+      .where('favorites.user_id', `${foreignKeys.user_id}`)
+      .where('favorites.drink_id', `${foreignKeys.drink_id}`);
+
+    if (duplicateCheckTable.length != 0) {
+      response.conflict('You have already favorited this drink');
+    } else {
+      const favorite = yield Favorite.create(Object.assign({}, attrs, foreignKeys));
+      yield favorite.related('user').load();
+
+      response.jsonApi('Favorite', favorite);
+    }
   }
 
   * show(request, response) {
@@ -42,8 +52,8 @@ class FavoriteController {
 
     const input = request.jsonApi.getAttributesSnakeCase(attributes);
     const foreignKeys = {
-      user_id: user,
-      drink_id: drink,
+      user_id: request.authUser.id,
+      drink_id: request.input('data.relationships.drink.data.id'),
     };
 
     const favorite = yield Favorite.with('user', 'drink').where({ id }).firstOrFail();
